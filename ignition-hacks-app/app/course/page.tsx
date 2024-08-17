a87f80fe80aafeee6ecf0c6e1ed0934e3b64c542
tjessica13 <jtanottawau@gmail.com>
2024-08-17 17:52:40 -0400

    adding course page content

--- ignition-hacks-app/app/course/page.tsx
+++ ignition-hacks-app/app/course/page.tsx
@@ -1,7 +1,50 @@
-
+import { SideBar, Footer, Home, NavBar } from '../../components/Components';
 
 export default function Course() {
     return (
-        <div> Course </div>
+        <div className="grid grid-rows-10">
+
+      <div className="row-span-8 flex space-x-6">
+
+        <div className="w-1/6">
+            <SideBar />
+        </div>
+
+
+        <div className="w-5/6">
+            <div className="flex space-x-4">
+
+              <div id="chat" className="w-full">
+                <ul>
+                  <li id="bot-message">
+                    <p><b>Android v5.5</b></p>
+                    <p>Are you ready to learn about AI?</p>
+                  </li>
+
+                  <li id="user-message">
+                    <p><b>User</b></p>
+                    <p>Yes I am ready!</p>
+                  </li>
+                </ul>
+              </div>
+
+              <div id="tutor-bot" className="w-full">
+                <img src="img/chatbot-pngtuber.png" />
+              </div>
+            </div>
+            
+        </div>
+      </div>
+
+      <div id="chat-box" className="flex row-span-2 justify-center">
+          <form className="px-2 m-5 h-10 justify-center">
+            <input type="text" className="min-w-full" placeholder="Ask your questions here"></input>
+            <input type="submit" value="Send" className="mx-2 py-1 px-4 bg-black text-white"></input>
+          </form>
+      </div>
+
+      <Footer />
+    </div>
+
     );
 }
